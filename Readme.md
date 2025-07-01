a06ff9c24950d02d1a692f0834ab1d93a69e58d1
Jovito Isorena Jr <jovito.isorena@GXCHLAP00705.local>
2025-07-01 15:37:58 +0800

    Final Submission, FECP-Java-Session1-ControlStructures

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# Name 
+Jovito Isorena
+# Project Title 
+FECP-Java-Session1-ControlStructures
+# Screenshot of your output
+![image info](./java-lab2-1.png)
+![image info](./java-lab2-2.png)
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,48 @@ import java.util.*;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String membership;
+        int age;
+        float price;
+        System.out.print("Enter membership status (Regular, VIP, Premium): ");
+        membership = sc.nextLine();
+        System.out.print("Enter age: ");
+        age = sc.nextInt();
+        /* the price is $50 for users under 18,
+        $100 for users aged 18 to 64,
+        and $75 for users over 64.
+        */
+         if(membership.equalsIgnoreCase("regular")) {
+             if (age > 64) {
+                 price = 75.0f;
+             } else if (age < 18) {
+                 price = 50.0f;
+             } else {
+                 price = 100.0f;
+             }
+         }else if(membership.equalsIgnoreCase("vip")) {
+            if (age > 64) {
+                price = 112.5f;
+            } else if (age < 18) {
+                price = 75.0f;
+            } else {
+                price = 150.0f;
+            }
+        }else if(membership.equalsIgnoreCase("premium")) {
+            if (age > 64) {
+                price = 150.0f;
+            } else if (age < 18) {
+                price = 100.0f;
+            } else {
+                price = 200.0f;
+            }
+        }
+        else {
+            System.out.print("Invalid membership status entered.");
+            return;
+        }
+        System.out.printf("Price: $%.2f", price);
 
     }
 }
